3f5aa9caeb3c56895d0af953166ecb88840bcc9e
mstrow <38768048+mstrow@users.noreply.github.com>
2022-11-10 20:58:12 +1300

    Removed API Key

--- DataColabView/Models/Networking/JsnDrop.py
+++ DataColabView/Models/Networking/JsnDrop.py
@@ -2,9 +2,6 @@ import requests
 import json
 
 
-# cd5159dc-7fdf-40b7-82be-61a06a1de3e2
-# https://docs.python.org/3/library/json.html
-
 class jsnDrop(object):
 
     def __init__(self, tok=None, url=None) -> None:
@@ -47,34 +44,31 @@ class jsnDrop(object):
         return self.jsnResult
 
     def create(self, table_name, example):
-        # https://newsimland.com/~todd/JSON/?tok={"tok":"cd5159dc-7fdf-40b7-82be-61a06a1de3e2","cmd":{"CREATE":"tblTest","EXAMPLE":{"PersonID  PK":"Todd","Score":21}}}
+       
         command = self.jsnDropCreate
         command["CREATE"] = table_name
         command["EXAMPLE"] = example
         return self.jsnDropApi(command)
 
     def store(self, table_name, value_list):
-        # https://newsimland.com/~todd/JSON/?tok={"tok":"cd5159dc-7fdf-40b7-82be-61a06a1de3e2","cmd":{"STORE":"tblTest","VALUE":[{"PersonID":"Todd","Score":21},{"PersonID":"Jane","Score":2000}]}}
+        
         command = self.jsnDropStore
         command["STORE"] = table_name
         command["VALUE"] = value_list
         return self.jsnDropApi(command)
 
     def all(self, table_name):
-        # https://newsimland.com/~todd/JSON/?tok={"tok":"cd5159dc-7fdf-40b7-82be-61a06a1de3e2","cmd":{"ALL":"tblTest"}}
         command = self.jsnDropAll
         command["ALL"] = table_name
         return self.jsnDropApi(command)
 
     def select(self, table_name, where):
-        # https://newsimland.com/~todd/JSON/?tok={"tok":"cd5159dc-7fdf-40b7-82be-61a06a1de3e2","cmd":{"SELECT":"tblTest","WHERE":"Score > 200"}}
         command = self.jsnDropSelect
         command["SELECT"] = table_name
         command["WHERE"] = where
         return self.jsnDropApi(command)
 
     def delete(self, table_name, where):
-        # https://newsimland.com/~todd/JSON/?tok={"tok":"cd5159dc-7fdf-40b7-82be-61a06a1de3e2","cmd":{"DELETE":"tblTest","WHERE":"Score > 200"}}
         command = self.jsnDropDelete
         command["DELETE"] = table_name
         command["WHERE"] = where
